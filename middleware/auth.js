6ddf23a0703ece32b3578ec21c410c4f7926c0f4
SuperBlockchain Pool <55582476+SuperBlockchain-Pool@users.noreply.github.com>
2026-03-06 10:23:13 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,16 +3,16 @@ require("dotenv").config();
 
 module.exports = function (req, res, next) {
   const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.status(401).json({ error: "Token requerido" });
+  if (!authHeader) return res.status(401).json({ error: "Token required" });
 
   const token = authHeader.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "Token inválido" });
+  if (!token) return res.status(401).json({ error: "Invalid token" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ error: "Token inválido" });
+    return res.status(401).json({ error: "Invalid token" });
   }
 };
